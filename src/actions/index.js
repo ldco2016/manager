3712e1ade570a50eda950d36c132b4da5a95684c
danale <danale@danales-MacBook-Pro.local>
2018-09-24 08:02:02 -0400

    Making LoginUser more robust

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,27 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 export const emailChanged = text => {
   return {
-    type: 'EMAIL_CHANGED',
+    type: EMAIL_CHANGED,
     payload: text
   };
 };
 
 export const passwordChanged = text => {
   return {
-    type: 'PASSWORD_CHANGED',
+    type: PASSWORD_CHANGED,
     payload: text
   };
 };
 
 export const loginUser = ({ email, password }) => {
-  return (dispatch) => {
+  return dispatch => {
     firebase
       .auth()
-      .signInWithEmailAndPassword(email, pasword)
-      .then(user => console.log(user));
+      .signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      });
   };
 };
