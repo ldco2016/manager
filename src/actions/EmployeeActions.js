0c3ab77a32c7175d848a1fdbf2cb4c7c93c3fcc3
danale <danale@danales-MacBook-Pro.local>
2019-01-15 16:01:41 -0600

    Configured firebase as a datastore

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase';
-
-import { EMPLOYEE_UPDATE } from './types';
+import { Actions } from 'react-native-router-flux';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+} from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return { type: EMPLOYEE_UPDATE, payload: { prop, value } };
@@ -9,8 +13,29 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
 
-  firebase
-    .database()
-    .ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      // add a .then() to ensure after employee is created
+      // we want to navigate them back to employee list screen
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.pop();
+      });
+  };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
+  };
 };
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -10,6 +10,8 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMPLOYEE_UPDATE:
       return { ...state, [action.payload.prop]: action.payload.value };
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
     default:
       return state;
   }
